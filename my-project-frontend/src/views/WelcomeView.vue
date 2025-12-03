ef1820b6297019709e21d81833852086708acc3c
ZZYandZY <18827561151@163.com>
2025-12-03 17:53:33 +0800

    完成

---
+++ my-project-frontend/src/views/WelcomeView.vue
@@ -0,0 +1,40 @@
+<template>
+    <div style="width: 100vw;height: 100vh;overflow: hidden;display: flex">
+        <div style="flex: 1">
+            <el-image style="width: 100%;height: 100%" fit="cover"
+                      src="lanyangyang.jpg"/>
+        </div>
+        <div class="welcome-title">
+            <div style="font-size: 30px;font-weight: bold">欢迎来到我们的学习平台</div>
+            <div style="margin-top: 10px">在这里你可以学习如何使用Java，如何搭建网站。</div>
+            <div style="margin-top: 5px">在这里你可以一起玩金铲铲。</div>
+        </div>
+        <div class="right-card">
+            <router-view v-slot="{ Component }">
+                <transition name="el-fade-in-linear" mode="out-in">
+                    <component :is="Component" style="height: 100%"/>
+                </transition>
+            </router-view>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.right-card {
+  width: 400px;
+  z-index: 1;
+  background-color: var(--el-bg-color);
+}
+
+.welcome-title {
+    position: absolute;
+    bottom: 30px;
+    left: 30px;
+    color: white;
+    text-shadow: 0 0 10px black;
+}
+</style>
