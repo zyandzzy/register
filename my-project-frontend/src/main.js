a627f56de10105cb05ce21f78326666357dac7c8
ZZYandZY <18827561151@163.com>
2025-12-04 23:11:07 +0800

    用户基本信息加载和首页顶部完成

--- my-project-frontend/src/main.js
+++ my-project-frontend/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from "axios";
-
+import {createPinia} from "pinia";
 import 'element-plus/theme-chalk/dark/css-vars.css'
 
 axios.defaults.baseURL = 'http://localhost:8081'
@@ -10,5 +10,5 @@ axios.defaults.baseURL = 'http://localhost:8081'
 const app = createApp(App)
 
 app.use(router)
-
+app.use(createPinia())
 app.mount('#app')
--- my-project-frontend/src/views/IndexView.vue
+++ my-project-frontend/src/views/IndexView.vue
@@ -1,18 +1,82 @@
-<template>
-  <div>
-    <el-button @click="userLogout">退出登录</el-button>
-  </div>
-</template>
-
 <script setup>
-import { logout } from '@/net'
+import {get, logout} from '@/net'
 import router from "@/router";
+import {useStore} from "@/store";
+import {ref} from "vue";
+
+const store = useStore()
+const loading = ref(true)
+
+get("api/user/info", (data) => {
+  store.user = data
+  loading.value = false
+})
 
 function userLogout() {
   logout(() => router.push("/"))
 }
 </script>
 
-<style scoped>
+<template>
+  <div class="main-content" v-loading="loading" element-loading-text="正在加载,请稍后···">
+    <el-container style="height: 100vh" v-if="!loading">
+      <el-header class="main-content-header">
+        <el-image class="logo" src="https://th.bing.com/th/id/OSAAS.2AF8D47DEA75F028F377DABC19B3040C?w=72&h=72&c=17&rs=1&o=6&dpr=1.3&pid=TechQna"></el-image>
+        <div style="flex: 1" class="user-info">
+          <div class="profile">
+            <div>{{store.user.username}}</div>
+            <div>{{store.user.email}}</div>
+          </div>
 
+          <el-avatar src="https://www.itbaima.cn/image/avatar/default.webp"></el-avatar>
+        </div>
+
+      </el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+
+<style lang="less" scoped>
+.main-content {
+  height: 100vh;
+  width: 100vw;
+}
+
+.main-content-header {
+  border-bottom: solid 1px var(--el-border-color);
+  height: 75px;
+  display: flex;
+  align-items: center;
+  box-sizing: border-box;
+
+  .logo {
+    height: 55px;
+  }
+
+  .user-info {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+  }
+
+  .profile {
+    text-align: right;
+    margin-right: 20px;
+
+    :first-child {
+      font-weight: bold;
+      font-size: 25px;
+      line-height: 30px;
+    }
+    :last-child {
+      font-size: 18px;
+      color: grey;
+    }
+  }
+}
 </style>
